192ee06f817717ffcfd3db24c4d59f73439ba92d
Zeke Medley <zekemedley@berkeley.edu>
2019-04-29 16:02:28 -0700

    js should work as expected now...

--- scripts/line-wrapping.js
+++ scripts/line-wrapping.js
@@ -1,77 +1,62 @@
-/*
-Enforces hard line wrapping on words.
-*/
-function enforceWords(text, len) {
-  var lines = text.split(/\n/);
-  var goodLines = [];
-  var i;
-  for (i = 0; i < lines.length; i++) {
-    var words = lines[i].split(' ');
-    /*
-    If a word would cause you to excede the line width,
-    just put it there anyway. This is how nano does it.
-    */
-    var goodStrings = [words[0]];
-    var lineLen = words[0].length;
-    var j;
-    for (j = 1; j < words.length; j++) {
-      cannidateLength = words[j].length;
-      if (lineLen + cannidateLength > len) {
-        goodLines.push(goodStrings.join(' ').trim());
-        goodStrings = [words[j]];
-        lineLen = words[j].length;
-      } else {
-      goodStrings.push(words[j]);
-      lineLen += words[j].length;
-      }
+function enforceAndGrowWords(text, len) {
+  // Replace only single newlines ignoring doubles.
+  splits = text.split(/\n\n/g);
+  let i;
+  for (i = 0; i < splits.length; i++) {
+    section = splits[i];
+    section = section.replace(/\n/g, " ");
+    words = section.split(" ");
+    splits[i] = wrapWords(words, len);
+  }
+  return splits.join("\n\n");
+}
+
+function wrapWords(words, len) {
+  let goodLines = [];
+  while (words.length > 0) {
+    let line = []
+    while (totalLen(line) < len && words.length > 0) {
+      line.push(words.shift());
     }
-    if (goodStrings.length == 0) {
-      goodLines.push("\n");
-    } else {
-      // Add the trailing good strings
-      goodLines.push(goodStrings.join(' '));
-    }
+    goodLines.push(line.join(" "));
   }
   return goodLines.join("\n");
 }
 
-function enforceAndGrowWords(text, len) {
-  text = text.replace(/\n+/g, function($0) {
-        return $0.length == 1 ? ' ' : $0;
-    });
-  var words = text.split(" ");
-  var goodStrings = [words[0]];
-  var goodLines = [];
-  var lineLen = words[0].length;
-  var j;
-  for (j = 1; j < words.length; j++) {
-    cannidateLength = words[j].length;
-    if (lineLen + cannidateLength > len) {
-      goodLines.push(goodStrings.join(' ').trim());
-      goodStrings = [words[j]];
-      lineLen = words[j].length;
-    } else {
-    goodStrings.push(words[j]);
-    lineLen += words[j].length;
-    }
+function totalLen(list) {
+  let i;
+  let l = 0;
+  for (i = 0; i < list.length; i++) {
+    l += list[i].length;
   }
-  // Add the trailing good strings
-  goodLines.push(goodStrings.join(' '));
-  return goodLines.join("\n");
+  return l;
+}
+
+// handle enter plain javascript
+function handleEnter(e){
+    let keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+      // Enter key has been pressed & textbox is focused
+      if ($("#hardtext").is(':focus')) {
+        $('#hardtext').val($('#hardtext').val() + "\n");
+      }
+    }
 }
 
 $(document).ready(function(){
   var slider = document.getElementById("custom-handle");
   var value = document.getElementById("slider-value");
   var maxLength = slider.value;
+  value.innerHTML = maxLength;
+  window.totalEndNewLines = 0;
   slider.oninput = function() {
     maxLength = slider.value;
     value.innerHTML = maxLength;
     $('#hardtext').val(enforceAndGrowWords($('#hardtext').val(), maxLength));
   }
   $('#hardtext').on('input focus keydown keyup', function() {
-      var text = $(this).val();
-      lines = enforceWords(text, maxLength)
+      let text = $(this).val();
+      lines = enforceAndGrowWords(text, maxLength)
       $(this).val(lines);
   });
 });
